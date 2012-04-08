22e06ce7fe152f3f76f37d2c17e96785fb7204c8
Juliana Pena <limited.mage@gmail.com>
2012-04-07 22:26:00 -0500

    Getting pretty :D

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
+var editor;
+
 function __clear() {
-        $('#debug2').val("");
-        $('#code tbody tr').remove();
+  $('#debug2').val("");
+  $('#code tbody tr').remove();
 }
 
 function __update(name, val, line) {
-        var len = $('#code tbody tr').length;
-        for (var i = len; i <= line; i++) {
-                $('#code tbody').append($('<tr></tr>'));
-        }
-        $($('#code tbody tr')[line]).append($('<td>' + name + ' = ' + JSON.stringify(val) + '</td>'));
+  var len = $('#code tbody tr').length;
+  for (var i = len; i <= line; i++) {
+    $('#code tbody').append($('<tr></tr>'));
+  }
+  $($('#code tbody tr')[line]).append($('<td>' + name + ' = ' + JSON.stringify(val) + '</td>'));
 }
 
 function __postchange(name, val, inc, line) {
-        $('#debug2').val($('#debug2').val() + '\n' + name + ' = ' + (val + inc) + "(" + (line + 1) + ")");
-        __update(name, val + inc, line);
-        return val;
+  $('#debug2').val($('#debug2').val() + '\n' + name + ' = ' + (val + inc) + "(" + (line + 1) + ")");
+  __update(name, val + inc, line);
+  return val;
 }
 
 function __change(name, val, line) {
-        $('#debug2').val($('#debug2').val() + '\n' + name + ' = ' + JSON.stringify(val) + "(" + (line + 1) + ")");
-        __update(name, val, line);
-        return val;
+  $('#debug2').val($('#debug2').val() + '\n' + name + ' = ' + JSON.stringify(val) + "(" + (line + 1) + ")");
+  __update(name, val, line);
+  return val;
 }
 
 function __loopstart() {
-        $('#debug2').val($('#debug2').val() + '\n' + "{");
+  $('#debug2').val($('#debug2').val() + '\n' + "{");
 }
 
 function __loopend() {
-        $('#debug2').val($('#debug2').val() + '\n' + "}");
+  $('#debug2').val($('#debug2').val() + '\n' + "}");
 }
 
 function __loop() {
-        $('#debug2').val($('#debug2').val() + '\n' + "continue;");
-        return true;
+  $('#debug2').val($('#debug2').val() + '\n' + "continue;");
+  return true;
 }
 
 function update() {
-        var src = jsparser.parse($('#source').val());
-        $('#debug').val(src);
-        __clear();
-        eval(src);
+  var src = jsparser.parse(editor.getValue());
+  $('#debug').val(src);
+  __clear();
+  eval(src);
 }
 
 $(document).ready(function () {
-        update();
-        $('#source').keyup(update);
+  editor = CodeMirror.fromTextArea(document.getElementById("source"), {
+    lineNumbers: true,
+    matchBrackets: true,
+    onChange: update,
+    tabSize: 4
+  });
+
+  update();
+  
+  $('.CodeMirror-lines pre:not(.CodeMirror-cursor), #code tr').live('mouseover', function() {
+    var index = $(this).index() + 1;
+    $('#code tr:nth-child('+index+')').addClass('hover');
+    var other = $('.CodeMirror-lines > div > div > pre:nth-child('+index+')');
+    other.addClass('hover');
+  });
+  
+  $('.CodeMirror-lines pre:not(.CodeMirror-cursor), #code tr').live('mouseout', function() {
+    var index = $(this).index() + 1;
+    $('#code tr:nth-child('+index+')').removeClass('hover');
+    $('.CodeMirror-lines > div > div > pre:nth-child('+index+')').removeClass('hover');
+  });
 });
+
